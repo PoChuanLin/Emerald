3644f6262cfe28db74c4642bdd636c354829bd8b
Po-Chuan Lin <pclin.java@gmail.com>
2020-09-16 07:50:21 -0500

    add new Exception handler

---
+++ src/main/java/net/nextgen/emerald/controller/EnrolleeNotFoundAdvice.java
@@ -0,0 +1,18 @@
+package net.nextgen.emerald.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class EnrolleeNotFoundAdvice {
+
+    @ResponseBody
+    @ExceptionHandler(EnrolleeNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String enrolleeNotFoundHandler(EnrolleeNotFoundException ex) {
+        return ex.getMessage();
+    }
+}
