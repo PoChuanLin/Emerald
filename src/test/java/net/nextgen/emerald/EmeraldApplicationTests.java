2f2b49dded431c915bf2e9fbbedfbfec4b569a31
Po-Chuan Lin <pclin.java@gmail.com>
2020-09-15 22:10:17 -0500

    project initial commit

---
+++ src/test/java/net/nextgen/emerald/EmeraldApplicationTests.java
@@ -0,0 +1,12 @@
+package net.nextgen.emerald;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmeraldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
