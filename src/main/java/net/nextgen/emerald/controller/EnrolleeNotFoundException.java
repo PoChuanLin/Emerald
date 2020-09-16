37ed36aa3e4bdb2c35b6f63c77c419b188555b95
Po-Chuan Lin <pclin.java@gmail.com>
2020-09-16 07:53:04 -0500

    minor - fix typo in text

--- src/main/java/net/nextgen/emerald/controller/EnrolleeNotFoundException.java
+++ src/main/java/net/nextgen/emerald/controller/EnrolleeNotFoundException.java
@@ -2,6 +2,6 @@ package net.nextgen.emerald.controller;
 
 public class EnrolleeNotFoundException extends RuntimeException {
     EnrolleeNotFoundException(Long id) {
-        super("Cound not find enrollee "+id);
+        super("Could not find enrollee "+id);
     }
 }
