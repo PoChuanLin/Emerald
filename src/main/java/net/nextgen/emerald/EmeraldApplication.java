a062afb5938eb9a2db10645b5f3de37efd47fe03
Po-Chuan Lin <pclin.java@gmail.com>
2020-09-17 12:04:43 -0500

    minor - indent chanage

--- src/main/java/net/nextgen/emerald/EmeraldApplication.java
+++ src/main/java/net/nextgen/emerald/EmeraldApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmeraldApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(EmeraldApplication.class, args);
+        SpringApplication.run(EmeraldApplication.class, args);
     }
 
 }
