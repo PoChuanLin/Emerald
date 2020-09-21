4b130fbab88794e14c85f30c974afc42645b7009
Po-Chuan Lin <pclin.java@gmail.com>
2020-09-21 15:14:18 -0500

    add class : EnrolleeService

--- src/main/java/net/nextgen/emerald/controller/EnrolleeController.java
+++ src/main/java/net/nextgen/emerald/controller/EnrolleeController.java
@@ -5,56 +5,44 @@ import javax.inject.Inject;
 
 import org.springframework.web.bind.annotation.*;
 
-import net.nextgen.emerald.dao.EnrolleeRepository;
-import net.nextgen.emerald.service.EnrolleeNotFoundException;
+import net.nextgen.emerald.service.EnrolleeService;
 import net.nextgen.emerald.vo.Enrollee;
 
 @RestController
 public class EnrolleeController {
 
     @Inject
-    private EnrolleeRepository repository;
-
-    @GetMapping("/enrollees")
-    List<Enrollee> all() {
-        return repository.findAll();
-    }
-
-    @GetMapping("/enrollees/{id}")
-    Enrollee one(@PathVariable Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new EnrolleeNotFoundException(id));
-    }
+    private EnrolleeService enrolleeService;
 
     /* Add a new enrollee */
 
     @PostMapping("/enrollees")
     Enrollee newEnrollee(@RequestBody Enrollee newEnrollee) {
-        return repository.save(newEnrollee);
+        return enrolleeService.create(newEnrollee);
+    }
+
+    @GetMapping("/enrollees")
+    List<Enrollee> all() {
+        return enrolleeService.read();
+    }
+
+    @GetMapping("/enrollees/{id}")
+    Enrollee one(@PathVariable Long id) {
+        return enrolleeService.read(id);
     }
 
     /* Modify an existing enrollee */
 
     @PutMapping("/enrollees/{id}")
-    Enrollee replaceEnrollee(@RequestBody Enrollee newEnrollee, @PathVariable Long id) {
-        return repository.findById(id)
-                .map(enrollee -> {
-                    enrollee.setName(newEnrollee.getName());
-                    enrollee.setActivation(newEnrollee.getActivation());
-                    enrollee.setDob(newEnrollee.getDob());
-                    enrollee.setPhone(newEnrollee.getPhone());
-                    return repository.save(enrollee);
-                })
-                .orElseGet(() -> {
-                    newEnrollee.setId(id);
-                    return repository.save(newEnrollee);
-                });
+    Enrollee replaceEnrollee(@PathVariable Long id, @RequestBody Enrollee newEnrollee) {
+        return enrolleeService.update(id, newEnrollee);
     }
 
     /* Remove an enrollee entirely */
 
+    /* The enrollee and all associated dependents will be deleted */
     @DeleteMapping("/enrollees/{id}")
     void deleteEnrollee(@PathVariable Long id) {
-        repository.deleteById(id);
+        enrolleeService.delete(id);
     }
 }
---
+++ src/main/java/net/nextgen/emerald/service/EnrolleeService.java
@@ -0,0 +1,64 @@
+package net.nextgen.emerald.service;
+
+import net.nextgen.emerald.dao.DependentRepository;
+import org.springframework.stereotype.Service;
+
+import net.nextgen.emerald.dao.EnrolleeRepository;
+import net.nextgen.emerald.vo.Enrollee;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**  Services for Enrollee persistence, CRUD processes.
+ */
+@Service
+public class EnrolleeService {
+    @Inject
+    private EnrolleeRepository enrolleeRepository;
+    @Inject
+    private DependentRepository dependentRepository;
+
+    /* Create */
+
+    public Enrollee create(Enrollee newEnrollee) {
+        return enrolleeRepository.save(newEnrollee);
+    }
+
+    /* Retrieve */
+
+    public List<Enrollee> read() {
+        return enrolleeRepository.findAll();
+    }
+
+    public Enrollee read(Long id) {
+        return enrolleeRepository.findById(id)
+                .orElseThrow(() -> new EnrolleeNotFoundException(id));
+    }
+
+    /* Update */
+
+    public Enrollee update (Long id, Enrollee newEnrollee) {
+        return enrolleeRepository.findById(id)
+                .map(enrollee -> {
+                    enrollee.setName(newEnrollee.getName());
+                    enrollee.setActivation(newEnrollee.getActivation());
+                    enrollee.setDob(newEnrollee.getDob());
+                    enrollee.setPhone(newEnrollee.getPhone());
+                    return enrolleeRepository.save(enrollee);
+                })
+                .orElseGet(() -> {
+                    newEnrollee.setId(id);
+                    return enrolleeRepository.save(newEnrollee);
+                });
+    }
+
+    /* Delete */
+
+    /** Delete an Enrollee by its ID, along with all its dependents.
+     * @param id ID of the Enrollee, along with all its dependents, to be deleted.
+     */
+    public void delete(Long id) {
+        dependentRepository.deleteByEnrolleeId(id);
+        enrolleeRepository.deleteById(id);
+    }
+}
