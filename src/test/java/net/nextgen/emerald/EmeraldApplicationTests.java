479d921dd83002130c1e1ae8c8814a8148cb82d1
Po-Chuan Lin <pclin.java@gmail.com>
2020-09-24 22:24:38 -0500

    add integration test.

--- src/test/java/net/nextgen/emerald/EmeraldApplicationTests.java
+++ src/test/java/net/nextgen/emerald/EmeraldApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled // duplicate function
+@Disabled // duplicate function and slow (8 sec)
 @SpringBootTest
 class EmeraldApplicationTests {
 
