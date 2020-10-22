1741078115e04fa9e8042fad41bb746e18e433e1
Po-Chuan Lin <pclin.java@gmail.com>
2020-10-22 16:27:03 -0500

    enhance to handle all Spring MVC exceptions

--- src/main/java/net/nextgen/emerald/controller/ErrorHandlingControllerAdvice.java
+++ src/main/java/net/nextgen/emerald/controller/ErrorHandlingControllerAdvice.java
@@ -3,24 +3,40 @@ package net.nextgen.emerald.controller;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import net.nextgen.emerald.service.DependentNotFoundException;
 import net.nextgen.emerald.service.EnrolleeNotFoundException;
 
 @ControllerAdvice
-public class ErrorHandlingControllerAdvice {
-    
+public class ErrorHandlingControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid (MethodArgumentNotValidException ex,
+                                                                   HttpHeaders headers,
+                                                                   HttpStatus status,
+                                                                   WebRequest request) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            error.getViolations().add (new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return new ResponseEntity<>(error, headers, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler (ConstraintViolationException.class)
     @ResponseStatus (HttpStatus.BAD_REQUEST)
     @ResponseBody
-    ValidationErrorResponse onConstraintValidationException (ConstraintViolationException ex) {
+    ValidationErrorResponse handleConstraintValidationException (ConstraintViolationException ex) {
         ValidationErrorResponse error = new ValidationErrorResponse();
         for (ConstraintViolation violation : ex.getConstraintViolations()) {
             error.getViolations().add(new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
@@ -28,21 +44,10 @@ public class ErrorHandlingControllerAdvice {
         return error;
     }
 
-    @ExceptionHandler (MethodArgumentNotValidException.class)
-    @ResponseStatus (HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    ValidationErrorResponse onMethodArgumentNotValidException (MethodArgumentNotValidException ex) {
-        ValidationErrorResponse error = new ValidationErrorResponse();
-        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
-            error.getViolations().add (new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
-        }
-        return error;
-    }
-
     @ExceptionHandler (EnrolleeNotFoundException.class)
     @ResponseStatus (HttpStatus.NOT_FOUND)
     @ResponseBody
-    ValidationErrorResponse onEnrolleeNotFoundException (EnrolleeNotFoundException ex) {
+    ValidationErrorResponse handleEnrolleeNotFoundException (EnrolleeNotFoundException ex) {
         ValidationErrorResponse error = new ValidationErrorResponse();
         error.getViolations().add (new Violation(ex.getClass().getSimpleName(), ex.getMessage()));
         return error;
@@ -51,7 +56,7 @@ public class ErrorHandlingControllerAdvice {
     @ExceptionHandler (DependentNotFoundException.class)
     @ResponseStatus (HttpStatus.NOT_FOUND)
     @ResponseBody
-    ValidationErrorResponse onDependentNotFoundException (DependentNotFoundException ex) {
+    ValidationErrorResponse handleDependentNotFoundException (DependentNotFoundException ex) {
         ValidationErrorResponse error = new ValidationErrorResponse();
         error.getViolations().add (new Violation(ex.getClass().getSimpleName(), ex.getMessage()));
         return error;
